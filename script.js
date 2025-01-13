0d0da1e80d203a9d8dece5835b3b4fe5666e4c4e
ADIL  MIRZA <adil.mirza2024@nst.rishihood.edu.in>
2025-01-13 14:59:14 +0530

    All done

--- script.js
+++ script.js
@@ -6,7 +6,8 @@
  
 
 
-// code for fixed-image(image that appear on hovering)
+function page4Animation(){
+    // code for fixed-image(image that appear on hovering)
 var elemC = document.querySelector("#elem-container")
 var fixed = document.querySelector("#fixed-image")
 elemC.addEventListener("mouseenter",function(){
@@ -30,14 +31,59 @@ elems.forEach(function(e){
     })
 })
 
+}
+
 
 // swiper js ka code 
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: "auto",
-    centeredSlides: true,
-    spaceBetween: 30,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-  });
+function swiperAnimation(){
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: "auto",
+        centeredSlides: true,
+        spaceBetween: 100,
+        
+      });
+}
+
+
+function menuAnimation(){
+// code for menu to become fade on clicking
+var menu = document.querySelector("nav h3")
+var full = document.querySelector("#full-scr")
+var navimg = document.querySelector("nav img")
+var flag = 0 
+menu.addEventListener("click", function(){
+    if(flag == 0){
+    full.style.top = 0
+    navimg.style.opacity = 0
+    flag = 1
+    }else{
+    full.style.top = "-100%"
+    navimg.style.opacity = 1
+    flag = 0  
+    }
+})
+   
+}
+
+function loaderAnimation(){
+    var loader = document.querySelector("#loader")
+    setTimeout(function(){
+    loader.style.top = "-100%"
+    },4000)
+
+}
+
+
+swiperAnimation()
+page4Animation()
+menuAnimation()
+loaderAnimation()
+
+
+
+
+
+
+
+
+
